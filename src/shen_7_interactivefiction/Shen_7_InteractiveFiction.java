e47abef62efffbfa0141468ce3bb12556731ab67
Xiangyu Shen <xshen2013@gmail.com>
2015-11-13 16:16:15 -0600

    Updates for interactive fiction Java code.

--- src/shen_7_interactivefiction/Shen_7_InteractiveFiction.java
+++ src/shen_7_interactivefiction/Shen_7_InteractiveFiction.java
@@ -23,9 +23,13 @@ public class Shen_7_InteractiveFiction {
     static boolean again = true;
     static int health = 80;
     static int supplies = 0;
+    static int moreSupplies;
     static int lumber = 0;
+    static int moreLumber;
     static int ducttape = 0;
+    static int moreTape;
     static int people = 0;
+    static int shelter = 0;
     static int winConditions = 0;
     static boolean win = false;
     static boolean carpenter = false;
@@ -41,8 +45,10 @@ public class Shen_7_InteractiveFiction {
             lumber = 0;
             ducttape = 0;
             people = 0;
+            shelter = 0;
             winConditions = 0;
             win = false;
+            carpenter = false;
             input = "";
         }
         exit();
@@ -62,6 +68,11 @@ public class Shen_7_InteractiveFiction {
         input = scan.nextLine();
         input = scan.nextLine();
         if (checkInput("island")) {
+            moreSupplies = rand.nextInt(4);
+            supplies = supplies + moreSupplies;
+            ducttape = ducttape + 1;
+            lumber = lumber + 2;
+            System.out.println("(+ " + moreSupplies + "Supplies) (+ 1 Duct Tape) (+ 2 Lumber) You wander deeper into the island, and find some strange fruit and building materials.");
             shelterOrFood();
         } else if (checkInput("ship")) {
             helpOrSelf();
@@ -76,15 +87,11 @@ public class Shen_7_InteractiveFiction {
     }
     
     private static void shelterOrFood() {
-        supplies = supplies + rand.nextInt(4);
-        ducttape = ducttape + 1;
-        lumber = lumber + 2;
-        System.out.println("(+ 1-3 Supplies) (+ 1 Duct Tape) (+ 2 Lumber) You wander deeper into the island, and find some strange fruit and building materials.");
         System.out.println("You realize it is getting late. You could start building a shelter from the surrounding foliage, or keep looking for food.");
         if (checkInput("shelter")) {
-            
+            firstShelter();
         } else if (checkInput("food")) {
-             
+             moarFood();
         } else {
             System.out.println("Not valid. Try again");
             shelterOrFood();
@@ -97,7 +104,7 @@ public class Shen_7_InteractiveFiction {
         if (checkInput("help")) {
             saveOne();
         } else if (checkInput("suppl")) {
-             
+            shipSupplies();
         } else {
             System.out.println("Not valid. Try again");
             helpOrSelf();
@@ -107,20 +114,66 @@ public class Shen_7_InteractiveFiction {
     public static void saveOne() {
         System.out.println("You enter the cabin, and see the carpenter and a crewman unconscious to the left. There are also five crewmen to the right trapped under a timber beam.");
         System.out.println("The ship is now tilting dangerously and you only have time to save one group.");
+        if (checkInput("left")) {
+            carpenter = true;
+            people = people + 1;
+            System.out.println("You rescue them and successfully escape.");
+            afterShip();
+        } else if (checkInput("right")) {
+            people = people + 5;
+            System.out.println("You rescue them and successfully escape.");
+            afterShip();
+        } else {
+            System.out.println("Not valid. Try again");
+            saveOne();
+        }
     }
     
     public static void shipSupplies() {
-        supplies = supplies + rand.nextInt(4) + 2;
+        moreSupplies = rand.nextInt(4) + 2;
+        supplies = supplies + moreSupplies;
         ducttape = ducttape + 1;
-        lumber = lumber + rand.nextInt(2) + 2;
-        System.out.println("(+ 2-5 Supplies) (+ 1 Duct tape) (+ 2-3 Lumber) You find some supplies. The ship is now tilting dangerously, so you run.");
+        moreLumber = rand.nextInt(2) + 2;
+        lumber = lumber + moreLumber;
+        System.out.println("(+ " + moreSupplies + "Supplies) (+ 1 Duct tape) (+ " + moreLumber + "Lumber) You find some supplies. The ship is now tilting dangerously, so you run.");
+        afterShip();
     }
     
     public static void afterShip() {
         System.out.println("After escaping the ship, you have a little bit of time to explore the island. Do you look for food or build a shelter?");
+        if (checkInput("shelter")) {
+            firstShelter();
+        } else if (checkInput("food")) {
+            moarFood();
+        } else {
+            System.out.println("Not valid. Try again");
+            shelterOrFood();
+        }
     }
     
+    private static void firstShelter() {
+        lumber = lumber - 2;
+        ducttape = ducttape - 1;
+        shelter = shelter + 1;
+        System.out.println("(- 2 Lumber) (- 1 Duct Tape) (+ 1 Shelter) You now have a small shelter. It is late, so you fall asleep.");
+        game();
+    }
+    
+    private static void moarFood() {
+        moreSupplies = rand.nextInt(3) + 2;
+        if (people >= 5) {
+            moreSupplies = rand.nextInt(4) + 4;
+        }
+        System.out.println("(+ " + moreSupplies + ") You find more food, but now it is late and you need to sleep.");
+        game();
+    }
+    
+    
+    
     private static void game() {
+        while ((winConditions < 10) & (health > 0)) {
+        //random event
+    }
         playAgain();
     }
     
@@ -134,12 +187,13 @@ public class Shen_7_InteractiveFiction {
         System.out.println("You have " + lumber + " lumber.");
         System.out.println("You have " + ducttape + " ducttape.");
         System.out.println("You have " + people + " people.");
+        System.out.println("You have " + shelter + " shelter.");
     }
     
     private static boolean checkInput(String check){
         return input.toLowerCase().contains(check);
     }
-    
+      
     private static void playAgain() {
         System.out.println("Game over.");
         System.out.println("Would you like to play again? (Y/N)");
